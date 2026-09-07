285c0b51004c2334173fa07461ec10cdcc1cb1a1
Tausif Alam <123tausifalam123@gmail.com>
2026-09-07 23:45:25 +0530

    add entity fields

--- src/main/java/com/tausif/CargoZen_Backend/entity/Customer.java
+++ src/main/java/com/tausif/CargoZen_Backend/entity/Customer.java
@@ -1,4 +1,30 @@
 package com.tausif.CargoZen_Backend.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String name;
+    private String phone;
+    private String password;
+
+    private LocalDateTime cratedAt;
+
+
 }
