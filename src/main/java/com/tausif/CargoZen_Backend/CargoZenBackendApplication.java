62559f57523a18bd54fa0e52a5959bd4087cf4f6
Tausif Alam <123tausifalam123@gmail.com>
2026-09-07 23:37:46 +0530

    initial commit

---
+++ src/main/java/com/tausif/CargoZen_Backend/CargoZenBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tausif.CargoZen_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargoZenBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargoZenBackendApplication.class, args);
+	}
+
+}
