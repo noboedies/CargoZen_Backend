d85b825b24101c7344618ef521ab564957b79733
Tausif Alam <123tausifalam123@gmail.com>
2026-09-07 23:48:18 +0530

    add Customer Repo requirement

--- src/main/java/com/tausif/CargoZen_Backend/repository/CustomerRepo.java
+++ src/main/java/com/tausif/CargoZen_Backend/repository/CustomerRepo.java
@@ -1,4 +1,10 @@
 package com.tausif.CargoZen_Backend.repository;
 
-public interface CustomerRepo {
+import com.tausif.CargoZen_Backend.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepo extends JpaRepository<Customer, Long> {
+
 }
