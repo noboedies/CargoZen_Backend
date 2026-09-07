e630721f97b1401ba50f62239b8766640a8c8d32
Tausif Alam <123tausifalam123@gmail.com>
2026-09-07 23:51:25 +0530

    autowire customerrepo

--- src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
+++ src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
@@ -1,7 +1,12 @@
 package com.tausif.CargoZen_Backend.service;
 
+import com.tausif.CargoZen_Backend.repository.CustomerRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CustomerService {
+
+    @Autowired
+    private CustomerRepo customerRepo;
 }
