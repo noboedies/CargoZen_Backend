384b7463470d5e1c90d1f96f9d8e2ae2d4b39f25
Tausif Alam <123tausifalam123@gmail.com>
2026-09-08 22:32:44 +0530

    add dto to entity mapping, add register logics and add jpa methods

--- src/main/java/com/tausif/CargoZen_Backend/repository/CustomerRepo.java
+++ src/main/java/com/tausif/CargoZen_Backend/repository/CustomerRepo.java
@@ -1,10 +1,18 @@
 package com.tausif.CargoZen_Backend.repository;
 
 import com.tausif.CargoZen_Backend.entity.Customer;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerRepo extends JpaRepository<Customer, Long> {
 
+    Object existsByEmail(@NotBlank @Email String email);
+
+    Customer findByEmail(@NotBlank @Email String email);
+
+    Customer findByUsername(@NotBlank @Size(min = 6, max = 10) String username);
 }
--- src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
+++ src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
@@ -1,11 +1,14 @@
 package com.tausif.CargoZen_Backend.service;
 
 import com.tausif.CargoZen_Backend.dto.CustomerRegisterDto;
+import com.tausif.CargoZen_Backend.entity.Customer;
 import com.tausif.CargoZen_Backend.repository.CustomerRepo;
 import jakarta.validation.Valid;
 import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 @Service
 public class CustomerService {
@@ -13,6 +16,29 @@ public class CustomerService {
     @Autowired
     private CustomerRepo customerRepo;
 
-    public @Nullable Boolean register(@Valid CustomerRegisterDto customerRegisterDto) {
+    public boolean register(CustomerRegisterDto customerRegisterDto) {
+        Customer customer = customerRepo.findByEmail(customerRegisterDto.getEmail());
+        if(customer == null){
+            customer = customerRepo.findByUsername(customerRegisterDto.getUsername());
+        }
+
+        if(customer != null){
+            return false;
+        }
+        customer = mapToEntity(customerRegisterDto);
+        customerRepo.save(customer);
+        return true;
+
+    }
+
+    private Customer mapToEntity(CustomerRegisterDto customerRegisterDto) {
+        Customer customer = new Customer();
+        customer.setName(customerRegisterDto.getName());
+        customer.setEmail(customerRegisterDto.getEmail());
+        customer.setUsername(customerRegisterDto.getUsername());
+        customer.setPassword(customerRegisterDto.getPassword());
+        customer.setPhone(customerRegisterDto.getPhone());
+        customer.setCratedAt(LocalDateTime.now());
+        return customer;
     }
 }
