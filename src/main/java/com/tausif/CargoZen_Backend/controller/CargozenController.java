bb7566801c978fa49e4f8a64e45f4f0aa2edbc47
Tausif Alam <123tausifalam123@gmail.com>
2026-09-08 22:06:01 +0530

    add register rest controller

--- src/main/java/com/tausif/CargoZen_Backend/controller/CargozenController.java
+++ src/main/java/com/tausif/CargoZen_Backend/controller/CargozenController.java
@@ -1,8 +1,13 @@
 package com.tausif.CargoZen_Backend.controller;
 
 
+import com.tausif.CargoZen_Backend.dto.CustomerRegisterDto;
 import com.tausif.CargoZen_Backend.service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,4 +15,10 @@ public class CargozenController {
 
     @Autowired
     private CustomerService customerService;
+
+    @PostMapping("/register")
+    public ResponseEntity<Boolean> register(@Valid  @RequestBody CustomerRegisterDto customerRegisterDto){
+        return ResponseEntity.ok(customerService.register(customerRegisterDto));
+    }
+
 }
--- src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
+++ src/main/java/com/tausif/CargoZen_Backend/service/CustomerService.java
@@ -1,6 +1,9 @@
 package com.tausif.CargoZen_Backend.service;
 
+import com.tausif.CargoZen_Backend.dto.CustomerRegisterDto;
 import com.tausif.CargoZen_Backend.repository.CustomerRepo;
+import jakarta.validation.Valid;
+import org.jspecify.annotations.Nullable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +12,7 @@ public class CustomerService {
 
     @Autowired
     private CustomerRepo customerRepo;
+
+    public @Nullable Boolean register(@Valid CustomerRegisterDto customerRegisterDto) {
+    }
 }
